4f0f43a2dd7947e1cdd838b2fd69dcb09c04e358
nakinton <r.nakinton@gmail.com>
2023-10-12 18:38:13 +0700

    update signup

--- createuser/forms.py
+++ createuser/forms.py
@@ -8,15 +8,16 @@ User = get_user_model()
 class createUserForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True, help_text='Required. Enter your first name.')
     last_name = forms.CharField(max_length=30, required=True, help_text='Required. Enter your last name.')
-
+    email = forms.EmailField(max_length = 200) 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name','email', 'password1', 'password2')
 
     def save(self, commit=True):
         user = super(createUserForm, self).save(commit=False)  # Use the correct class name
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
         scholar = Scholar(ID=user, scholar_name=user.get_full_name(), scholar_email=user.email)
         if commit:
             user.save()
